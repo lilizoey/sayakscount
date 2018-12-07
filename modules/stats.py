b535a6605a69537dbc1f31b9ff6c1255643b8408
sayaks <sayaks1@gmail.com>
2018-12-07 21:26:22 +0100

    Got a working stats command

--- modules/stats.py
+++ modules/stats.py
@@ -1,4 +1,10 @@
+import os
+import psutil
+import discord
+from datetime import datetime
 from bot import bot, database
+
+startup = datetime.now()
 
 INVITE = "https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=117760"
 
@@ -13,14 +19,23 @@ async def stats(ctx):
 
     username = f"{bot.user.name}#{bot.user.discriminator}"
     owner = f"{app_info.owner.name}#{app_info.owner.discriminator}"
-    servers = len(bot.guilds)
+    servers = bot.guilds
+    channels = sum([len(server.channels) for server in servers])
+    users = sum([server.member_count for server in servers])
+
 
     await ctx.send(f"""
 **Username**: {username}
 **Owner**: {owner}
-**Servers**: {servers}
+**Servers**: {len(servers)}
 **ID**: {app_info.id}
-**Library**: Discord.py rewrite
+**Library**: Discord.py {discord.__version__}
 **Database Entries**: {database.get_entries()}
+**Channels**: {channels}
+**Users**:  {users}
+**Creation Date**: {bot.user.created_at}
+**Join Date**: {ctx.message.guild.me.joined_at}
+**Uptime**: {datetime.now() - startup}
+**Memory Usage**: {str(round(float(psutil.Process(os.getpid()).memory_info().rss)/1048576, 1))+'MB'}
 """)
 
